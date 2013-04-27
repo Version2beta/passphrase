7a8c7ecb0d575560d330ce07dc1fcb578e170185
Rob Martin <rob@version2beta.com>
2013-04-27 17:22:21 -0500

    Added a setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='passphrase',
+    version='0.0.2',
+    author='Rob Martin @version2beta',
+    author_email='rob@version2beta.com',
+    packages=['passphrase'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/passphrase/',
+    license='LICENSE.txt',
+    description='List a series of words for selection of a secure and rememberable passphrase.'
+    long_description=open('README.md').read(),
+    install_requires=[],
+    package_data={
+        '': ['*.dist'],
+      },
+    entry_points={
+        'console_scripts': [
+          'passphrase = passphrase:main',
+        ],
+      },
+)
