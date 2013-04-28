7c5518b8aa6c1f4421aac7b8a652d737a17c4e86
Rob Martin <rob@version2beta.com>
2013-04-27 19:16:35 -0500

    Make it into a package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     scripts=[],
     url='http://pypi.python.org/pypi/passphrase/',
     license='LICENSE.txt',
-    description='List a series of words for selection of a secure and rememberable passphrase.'
+    description='List a series of words for selection of a secure and rememberable passphrase.',
     long_description=open('README.md').read(),
     install_requires=[],
     package_data={
